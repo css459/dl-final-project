1f169f6989815e73cfa7ba92cd3fef07f91d1005
Cole Smith <css@nyu.edu>
2020-04-28 22:46:25 -0400

    resvar experimental implementation

--- model/resnet.py
+++ model/resnet.py
@@ -23,12 +23,26 @@ class Prototype(nn.Module):
         # Backbone
         #
 
-        self.backbone, in_features = remove_backbone_head(resnet18(pretrained=False,
-                                                                   progress=True))
+        resnet = resnet18(pretrained=False, progress=True)
+        self.backbone, in_features = remove_backbone_head(resnet)
 
         # Translates the expected size of the backbone output to our
         # hidden size for later networks
         self.fc_translation_layer = nn.Linear(in_features, hidden_dim)
+
+        #
+        # Variational Layers
+        #
+        # These layers are used when the hidden encoding is to be variational
+        #
+
+        self.device = None
+        self.is_variational = False
+
+        z_dim = 512
+        self.fc1_var_encode = nn.Linear(hidden_dim, z_dim)
+        self.fc2_var_encode = nn.Linear(hidden_dim, z_dim)
+        self.fc3_var_decode = nn.Linear(z_dim, hidden_dim)
 
         #
         # Different "Heads" for which to fit onto the backbone
@@ -71,6 +85,7 @@ class Prototype(nn.Module):
         # OUTPUT: (batch size, 10, 400, 400)
         #          --> Call UpSample(2,2)
         #          --> (batch size, 10, 800, 800)
+        self.single_image_reconstructor_input_dim = 6144
         self.object_map_reconstructor = nn.Sequential(
             UnFlatten(input_size=6144),
 
@@ -103,16 +118,57 @@ class Prototype(nn.Module):
             Interpolate(scale_factor=(2, 2), mode='bilinear'),
         )
 
+    #
+    # Variational Functions
+    #
+
+    def reparameterize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        # return torch.normal(mu, std)
+        esp = torch.randn(*mu.size()).to(self.device)
+        z = mu + std * esp
+        return z
+
+    def bottleneck(self, h):
+        mu = self.fc1_var_encode(h)
+        logvar = self.fc2_var_encode(h)
+        z = self.reparameterize(mu, logvar)
+        return z, mu, logvar
+
+    #
+    # Encoders
+    #
+
     def backbone_encode(self, x):
         x = self.backbone(x)
         x = x.view(x.size(0), -1)
         x = self.fc_translation_layer(x)
         return x
 
+    def backbone_variational_encode(self, x):
+        x = self.backbone_encode(x)
+        z, mu, logvar = self.bottleneck(x)
+        return z, mu, logvar
+
+    def variational_decode(self, decode_fn, z):
+        z = self.fc3_var_decode(z)
+        return decode_fn(z)
+
+    #
+    # Forward Functions
+    #
+
     def single_image_forward(self, x):
         x = self.backbone_encode(x)
         x = self.single_image_reconstructor(x)
         return x
+
+    def single_image_variational_forward(self, x):
+        z, mu, logvar = self.backbone_variational_encode(x)
+        z = self.variational_decode(self.single_image_reconstructor, z)
+        z = torch.sigmoid(z)
+
+        return z, mu, logvar
 
     def object_map_forward(self, x):
         # Re-stack images to be
@@ -126,17 +182,69 @@ class Prototype(nn.Module):
             enc = self.backbone_encode(i)
             acc = torch.cat((acc, enc), 1)
 
+        if self.is_variational:
+            z, mu, logvar = self.backbone_variational_encode(x)
+
         acc = self.object_map_reconstructor(acc)
         return acc
+
+    def object_map_variational_forward(self, x):
+        # Re-stack images to be
+        # (6-directions, batch size, channels, H, W)
+        x = x.permute(1, 0, 2, 3, 4)
+
+        # print('x', x.shape)
+
+        # Encode all 6 images along the
+        # second dimension
+        z_acc, mu_acc, logvar_acc = self.backbone_variational_encode(x[0])
+
+        # print('z acc', z_acc.shape)
+        # print('mu acc', mu_acc.shape)
+        # print('logvar acc', logvar_acc.shape)
+
+        for i in x[1:]:
+            z, mu, logvar = self.backbone_variational_encode(i)
+            z_acc = torch.cat((z_acc, z), 1)
+            mu_acc = torch.cat((mu_acc, mu), 1)
+            logvar_acc = torch.cat((logvar_acc, logvar), 1)
+
+        # Combination layer for latent concatenation
+        # NOTE: Is this correct to do this?
+        fc_latent_translation = nn.Linear(z_acc.size(1),
+                                          self.hidden_dim * x.size(0)).to(self.device)
+        z_acc = fc_latent_translation(z_acc)
+
+        # print('z acc', z_acc.shape)
+        # print('mu acc', mu_acc.shape)
+        # print('logvar acc', logvar_acc.shape)
+
+        # Call the reconstructor directly, we've already done the FC as above
+        z_acc = self.object_map_reconstructor(z_acc)
+        z_acc = torch.sigmoid(z_acc)
+
+        # print('z acc dec', z_acc.shape)
+
+        return z_acc, mu_acc, logvar_acc
 
     def forward(self, x, mode='single-image'):
         assert mode in MODES
 
         if mode == 'single-image':
-            return self.single_image_forward(x)
+            if self.is_variational:
+                return self.single_image_variational_forward(x)
+            else:
+                return self.single_image_forward(x)
 
         if mode == 'object-map':
-            return self.object_map_forward(x)
+            if self.is_variational:
+                return self.object_map_variational_forward(x)
+            else:
+                return self.object_map_forward(x)
+
+    #
+    # Utility Functions
+    #
 
     @staticmethod
     def save(model, using_dataparallel=True, epoch_num=None, file_prefix='', save_dir='./resnet_weights'):
@@ -156,3 +264,16 @@ class Prototype(nn.Module):
         else:
             torch.save(model.state_dict(), full)
             torch.save(model.backbone.state_dict(), backbone)
+
+    # Reconstruction + KL divergence losses summed over all elements and batch
+    @staticmethod
+    def var_loss_function(recon_x, x, mu, logvar):
+        BCE = torch.nn.functional.binary_cross_entropy(recon_x, x, reduction='sum')
+
+        # see Appendix B from VAE paper:
+        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+        # https://arxiv.org/abs/1312.6114
+        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+        return BCE + KLD, BCE, KLD
---
+++ resvar-train.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from time import perf_counter
+
+import torch
+
+from data import get_unlabeled_set, get_labeled_set, set_seeds, make_bounding_box_images
+from model.resnet import Prototype
+
+#
+# Parameters
+#
+
+# The Resnet prototype will use variational
+# representations
+variational = True
+output_path = './resvar_weights/'
+
+batch_size = 32
+labeled_batch_size = 16
+hidden_size = 1024
+
+unlabeled_epochs = 20
+labeled_epochs = 20
+
+# Loads the Unlabeled-trained model from disk
+skip_unlabeled_training = False
+
+#
+# Setup
+#
+
+set_seeds()
+torch.backends.cudnn.benchmark = True
+
+if torch.cuda.is_available():
+    batch_size *= torch.cuda.device_count()
+
+#
+# Data
+#
+
+_, unlabeled_trainloader = get_unlabeled_set(batch_size=batch_size)
+(_, labeled_trainloader), (_, labeled_testloader) = get_labeled_set(batch_size=labeled_batch_size,
+                                                                    validation=0.2)
+
+#
+# Model
+#
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Prototype(hidden_dim=hidden_size)
+
+# Setup the variational parameters
+model.is_variational = variational
+model.device = device
+
+if skip_unlabeled_training:
+    model.load_state_dict(torch.load('./resvar_weights/unlabeled-resnet-latest.torch'))
+
+if torch.cuda.is_available():
+    model = torch.nn.DataParallel(model)
+    assert batch_size >= torch.cuda.device_count()
+    print('==> Using Data Parallel Devices:', torch.cuda.device_count())
+
+model = model.to(device)
+
+print('==> Device:', device)
+print('==> Batch Size:', batch_size)
+print('==> Unlabeled Epochs:', unlabeled_epochs)
+print('==> Labeled Epochs:', labeled_epochs)
+print('==> Hidden Encoding Size per Image:', hidden_size)
+print('==> Model Loaded. Begin Training')
+
+#
+# Unlabeled Pre-training
+#
+
+criterion = Prototype.var_loss_function
+optimizer = torch.optim.Adam(model.parameters())
+
+model.train()
+
+if not skip_unlabeled_training:
+    start_time = perf_counter()
+    for epoch in range(unlabeled_epochs):
+        loss = 0.0
+
+        max_batches = len(unlabeled_trainloader)
+        for idx, (images, camera_index) in enumerate(unlabeled_trainloader):
+            optimizer.zero_grad()
+
+            images = images.to(device)
+            reconstructions, mu, logvar = model(images, mode='single-image')
+            loss, bce, kld = criterion(reconstructions, images, mu, logvar)
+
+            loss.backward()
+            optimizer.step()
+
+            # Training Wheels
+            # print('loss', loss.item())
+            # break
+
+            if idx % 1000 == 0:
+                print('[', epoch, '|', idx, '/', max_batches, ']', 'loss:', loss.item(),
+                      'curr time mins:', round(int(perf_counter() - start_time) / 60, 2))
+
+        Prototype.save(model, file_prefix='unlabeled-', save_dir=output_path)
+
+    print('Unlabeled Training Took (Min):', round(int(perf_counter() - start_time) / 60, 2))
+
+#
+# Labeled Training
+#
+
+model.train()
+
+start_time = perf_counter()
+for epoch in range(labeled_epochs):
+    loss = 0.0
+
+    max_batches = len(labeled_trainloader)
+    for idx, (images, targets, _) in enumerate(labeled_trainloader):
+        optimizer.zero_grad()
+
+        images = torch.stack(images)
+        images = images.to(device)
+
+        # Rasterize bounding box images for reconstruction
+        targets = make_bounding_box_images(targets)
+        targets = targets.to(device)
+
+        # print('input shape:', images.shape)
+        # print('targt shape:', targets.shape)
+
+        reconstructions, mu, logvar = model(images, mode='object-map')
+
+        # print('outpt shape:', reconstructions.shape)
+
+        loss, bce, kld = criterion(reconstructions, targets, mu, logvar)
+
+        loss.backward()
+        optimizer.step()
+
+        # Training Wheels
+        # print('loss', loss.item())
+        # break
+
+        if idx % 1000 == 0:
+            print('[', epoch, '|', idx, '/', max_batches, ']', 'loss:', loss.item(),
+                  'curr time mins:', round(int(perf_counter() - start_time) / 60, 2))
+
+    Prototype.save(model, file_prefix='labeled-', save_dir=output_path)
+
+print('Labeled Training Took (Min):', round(int(perf_counter() - start_time) / 60, 2))
